1e8d32b6e1edf519a6347a408edac61e66a0c162
Taqwa <taqwaoumed@gmail.com>
2021-05-07 12:12:39 -0700

    Added SQL data

---
+++ data.sql
@@ -0,0 +1,10 @@
+INSERT INTO profile VALUES (1, 'Ramona', 'Brown', 'rbrown@hotmail.ca', 30, 'Nurse');
+INSERT INTO profile VALUES (2, 'Mary', 'Green', 'mgreen@hotmail.ca', 25, 'Manager');
+INSERT INTO profile VALUES (3, 'Donald', 'Turner', 'tdonald@hotmail.ca', 45, 'Hotel Manager');
+INSERT INTO profile VALUES (4, 'Brian', 'Hope', 'bhope@hotmail.ca', 35, 'Police Officer');
+INSERT INTO profile VALUES (5, 'Jennica', 'Nguyen', 'jenni@hotmail.ca', 25, 'Server');
+INSERT INTO profile VALUES (6, 'Mahmood', 'Hussain', 'mhussain@hotmail.ca', 65, 'Retired');
+INSERT INTO profile VALUES (7, 'Mahmood', 'Hussain', 'mhussain@hotmail.ca', 65, 'Retired');
+INSERT INTO profile VALUES (8, 'Mahmood', 'Hussain', 'mhussain@hotmail.ca', 65, 'Retired');
+INSERT INTO profile VALUES (9, 'Mahmood', 'Hussain', 'mhussain@hotmail.ca', 65, 'Retired');
+INSERT INTO profile VALUES (10, 'Mufasa', 'Mustafar', 'mmustafar@hotmail.ca', 38, 'Engineer');
