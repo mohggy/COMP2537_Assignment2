8686783f625bc68ab724d50d8dca65d7401c947c
ChunNok Ho <alexncho@hotmail.com>
2021-05-07 12:21:37 -0700

    Added my name and fixed some data

--- data.sql
+++ data.sql
@@ -4,7 +4,7 @@ INSERT INTO profile VALUES (3, 'Donald', 'Turner', 'tdonald@hotmail.ca', 45, 'Ho
 INSERT INTO profile VALUES (4, 'Brian', 'Hope', 'bhope@hotmail.ca', 35, 'Police Officer');
 INSERT INTO profile VALUES (5, 'Jennica', 'Nguyen', 'jenni@hotmail.ca', 25, 'Server');
 INSERT INTO profile VALUES (6, 'Mahmood', 'Hussain', 'mhussain@hotmail.ca', 65, 'Retired');
-INSERT INTO profile VALUES (7, 'Mahmood', 'Hussain', 'mhussain@hotmail.ca', 65, 'Retired');
-INSERT INTO profile VALUES (8, 'Mahmood', 'Hussain', 'mhussain@hotmail.ca', 65, 'Retired');
-INSERT INTO profile VALUES (9, 'Mahmood', 'Hussain', 'mhussain@hotmail.ca', 65, 'Retired');
+INSERT INTO profile VALUES (7, 'Nabil', 'Kirkland', 'nkirk@gmail.ca', 34, 'Truck driver');
+INSERT INTO profile VALUES (8, 'Joaquin', 'Fernandez', 'JoanquinF@hotmail.ca', 37, 'Actor');
+INSERT INTO profile VALUES (9, 'Sakura', 'Ito', 'SakuraIto@hotmail.com', 24, 'Designer');
 INSERT INTO profile VALUES (10, 'Mufasa', 'Mustafar', 'mmustafar@hotmail.ca', 38, 'Engineer');
